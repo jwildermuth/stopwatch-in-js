8186ff6ac0ad688b92c9543ab89117f450fc1c46
Jason Wildermuth <jmw33322@hotmail.com>
2021-08-07 14:02:52 -0400

    stopwatch: mostly working site, needs minor changes

--- stopwatch.js
+++ stopwatch.js
@@ -1,7 +1,7 @@
 let h = 0;
 let m = 0;
 let s = 0;
-let isRunning = false;
+let isRunning = null;
 let hours = document.getElementById('hour');
 let mins = document.getElementById('min');
 let secs = document.getElementById('sec');
@@ -9,28 +9,26 @@ let secs = document.getElementById('sec');
 window.onload = resetTimer();
 
 function updateDisplay() {
-  if (isRunning = true) {
-    hours.innerHTML = h + ':';
-    mins.innerHTML = m + ':';
-    secs.innerHTML = s;
-    console.log(h, m, s);
-    s++;
+  hours.innerHTML = h + ':';
+  mins.innerHTML = m + ':';
+  secs.innerHTML = s;
+  console.log(h, m, s);
+  s++;
   }
-}
 
 function resetTimer() {
-  isRunning = false;
-  hours.innerHTML = '00:';
-  mins.innerHTML = '00:';
-  secs.innerHTML = '00';
+  clearInterval(isRunning);
+  h, m, s = 0;
+  hours.innerHTML = '0:';
+  mins.innerHTML = '0:';
+  secs.innerHTML = '0';
 }
 
 function stopTimer() {
-  isRunning = false;
+  clearInterval(isRunning);
 }
 
 function runTimer() {
-  isRunning = true;
-  setInterval(() => updateDisplay(), 1000);
+  isRunning = setInterval(updateDisplay, 1000);
 }
 

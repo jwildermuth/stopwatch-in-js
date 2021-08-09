34a16146a784d9380564ad52224dff0f9f9ca576
Jason Wildermuth <jmw33322@hotmail.com>
2021-08-09 13:40:26 -0400

    Add hundredths of second counter

--- stopwatch.js
+++ stopwatch.js
@@ -1,10 +1,11 @@
 // jshint esversion: 6
 
-let h = m = s = 0;
+let h = m = s = c = 0;
 let isRunning = null;
 const hours = document.getElementById('hour');
 const mins = document.getElementById('min');
 const secs = document.getElementById('sec');
+const centis = document.getElementById('centi');
 const laplist = document.getElementById("laps");
 
 window.onload = resetTimer();
@@ -17,7 +18,12 @@ function clean(n, colon = false) {
 
 // Main timer function while running
 function updateDisplay() {
-  s++;
+  c++;
+  if (c == 100) {
+    c = 0;
+    s++;
+  }
+
   if (s == 60) {
     s = 0;
     m++;
@@ -30,7 +36,8 @@ function updateDisplay() {
 
   hours.innerHTML = clean(h, true);
   mins.innerHTML = clean(m, true);
-  secs.innerHTML = clean(s);
+  secs.innerHTML = clean(s, true);
+  centis.innerHTML = clean(c);
 }
 
 function clearLaps() {
@@ -42,10 +49,11 @@ function clearLaps() {
 
 function resetTimer() {
   clearInterval(isRunning);
-  h = m = s = 0;
+  h = m = s = c = 0;
   hours.innerHTML = clean(h, true);
   mins.innerHTML = clean(m, true);
-  secs.innerHTML = clean(s);
+  secs.innerHTML = clean(s, true);
+  centis.innerHTML = clean(c);
   clearLaps();
 }
 
@@ -57,7 +65,7 @@ function stopTimer() {
 // Add a lap time to the list
 function lapTime() {
   const lapNum = laplist.children.length + 1;
-  const now = `${clean(h, true)}${clean(m, true)}${clean(s)}`;
+  const now = `${clean(h, true)}${clean(m, true)}${clean(s, true)}${clean(c)}`;
   mark = `Lap ${lapNum}: ${now}`;
   const li = document.createElement('li');
   const txt = document.createTextNode(mark);
@@ -68,6 +76,6 @@ function lapTime() {
 // Start the timer using a setInterval
 function runTimer() {
   if (!isRunning) {
-    isRunning = setInterval(updateDisplay, 1000);
+    isRunning = setInterval(updateDisplay, 10);
   }
 }
